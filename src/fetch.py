3cb4039bd40638bea4e0dfd35e93f8c0d31b1fb6
seanson@users.noreply.github.com <seanson@users.noreply.github.com>
2021-01-21 22:59:01 +1100

    Initial commit

---
+++ src/fetch.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.channels.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+
+import os, pickle
+import json
+
+from pprint import pprint
+
+import google_auth_oauthlib.flow
+import google.auth.transport.requests
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+# TODO: Parameterise this for multiple playlists or channel IDs
+
+CHANNEL_ID = "UCcX7pakiWyBvFq15Vtb87wg"
+PLAYLIST_ID = "PLiHBUcQP9Nl4zO7cekAzFS-FU0fi3A3Gr"
+
+
+def youtube_auth():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = ".client_secret.json"
+
+    # Get credentials and create an API client
+    credentials = None
+    if os.path.exists(".token.pickle"):
+        with open(".token.pickle", "rb") as token:
+            credentials = pickle.load(token)
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            request = google.auth.transport.requests.Request()
+            credentials.refresh(request)
+        else:
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                client_secrets_file, scopes
+            )
+            credentials = flow.run_console()
+        with open(".token.pickle", "wb") as token:
+            pickle.dump(credentials, token)
+    return googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials
+    )
+
+
+def main():
+    youtube = youtube_auth()
+    request = youtube.playlistItems().list(
+        part="snippet",
+        playlistId=PLAYLIST_ID,
+        maxResults=250,
+    )
+    response = request.execute()
+    items = response.pop("items")
+    pprint(response)
+    while "nextPageToken" in response:
+        request = youtube.playlistItems().list(
+            part="snippet",
+            playlistId=PLAYLIST_ID,
+            maxResults=250,
+            # type="video",
+            # channelId=CHANNEL_ID,
+            # publishedAfter="2020-12-01T00:00:00Z",
+            pageToken=response["nextPageToken"],
+        )
+        response = request.execute()
+        print(len(response["items"]))
+        items += response.pop("items")
+        print(len(items))
+        pprint(response)
+    print("=" * 80)
+    print(f"Total items: {len(items)}")
+    items.reverse()
+    with open("results.json", "w") as results_file:
+        json.dump(items, results_file, indent=2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/generate.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+
+from copy import copy
+from pprint import pprint
+from jinja2 import Template
+
+heroes = {
+    "ana": {"name": "Ana", "matches": ["ana"]},
+    "ashe": {"name": "Ashe", "matches": ["ashe"]},
+    "baptiste": {"name": "Baptiste", "matches": ["baptiste", "bap"]},
+    "bastion": {"name": "Bastion", "matches": ["bastion"]},
+    "brigitte": {"name": "Brigitte", "matches": ["brig", "brigitte"]},
+    "dva": {"name": "D.Va", "matches": ["dva", "d.va"]},
+    "doomfist": {"name": "Doomfist", "matches": ["doom", "fist", "doomfist"]},
+    "echo": {"name": "Echo", "matches": ["echo"]},
+    "genji": {"name": "Genji", "matches": ["genji"]},
+    "hanzo": {"name": "Hanzo", "matches": ["hanzo"]},
+    "junkrat": {"name": "Junkrat", "matches": ["junk", "rat", "junkrat"]},
+    "lucio": {"name": "Lúcio", "matches": ["lucio"]},
+    "mccree": {"name": "McCree", "matches": ["mccree"]},
+    "mei": {"name": "Mei", "matches": ["mei"]},
+    "mercy": {"name": "Mercy", "matches": ["mercy"]},
+    "moira": {"name": "Moira", "matches": ["moira"]},
+    "orisa": {"name": "Orisa", "matches": ["orisa"]},
+    "pharah": {"name": "Pharah", "matches": ["pharah"]},
+    "reaper": {"name": "Reaper", "matches": ["reaper"]},
+    "reinhardt": {"name": "Reinhardt", "matches": ["reinhardt", "rein"]},
+    "roadhog": {"name": "Roadhog", "matches": ["roadhog", "hog"]},
+    "sigma": {"name": "Sigma", "matches": ["sigma"]},
+    "soldier": {"name": "Soldier: 76", "matches": ["soldier", "soldier:76"]},
+    "sombra": {"name": "Sombra", "matches": ["sombra"]},
+    "symmetra": {"name": "Symmetra", "matches": ["symmetra"]},
+    "torbjorn": {"name": "Torbjörn", "matches": ["torb", "torbjorn"]},
+    "tracer": {"name": "Tracer", "matches": ["tracer"]},
+    "widowmaker": {"name": "Widowmaker", "matches": ["widow", "widowmaker"]},
+    "winston": {"name": "Winston", "matches": ["winston", "ape", "monkey", "bubble"]},
+    "wreckingball": {
+        "name": "Wrecking Ball",
+        "matches": ["ball", "hammond", "hamster"],
+    },
+    "zarya": {"name": "Zarya", "matches": ["personalbubble", "zarya"]},
+    "zenyatta": {"name": "Zenyatta", "matches": ["zenyatta", "zen"]},
+}
+hero_match = {}
+for key, value in heroes.items():
+    for match in value["matches"]:
+        hero_match[match] = key
+
+comp_set = set(["qp", "bronze", "silver", "gold", "plat", "diamond"])
+
+
+def parse_results():
+    results = {}
+    results["heroes"] = copy(heroes)
+    results["comps"] = {key: [] for key in comp_set}
+
+    with open("results.json", "r") as results_file:
+        base = json.load(results_file)
+    for item in base:
+        title = (
+            item["snippet"]["title"]
+            .lower()
+            .replace("/", " ")
+            .replace("&#39;", "'")
+            .replace("soldier: 76", "soldier:76")
+            .replace("personal bubble", "personalbubble")
+            .replace("quick play", "qp")
+            .replace("'s", "")
+        )
+        title_tags = set(title.split(" "))
+        hero_tags = set()
+        comp_tags = comp_set.intersection(title_tags)
+        for tag in title_tags:
+            if tag in hero_match:
+                hero_tags.add(hero_match[tag])
+        item["heroes"] = hero_tags
+        item["comp"] = comp_tags
+        for hero in hero_tags:
+            results["heroes"][hero].setdefault("videos", [])
+            results["heroes"][hero]["videos"].append(item)
+        for comp in comp_tags:
+            results["comps"][comp].append(item)
+
+    return results
+
+
+def main():
+    results = parse_results()
+    with open("templates/index.html.jinja2", "r") as template_file:
+        template = Template(template_file.read())
+    with open("dist/index.html", "w") as html_file:
+        print("Writing index.html")
+        html = template.render(results=results, current_hero="ana")
+        html_file.write(html)
+    for current_hero in results["heroes"].keys():
+        print(f"Writing {current_hero}.html")
+        html = template.render(results=results, current_hero=current_hero)
+        with open(f"dist/{current_hero}.html", "w") as html_file:
+            html_file.write(html)
+
+
+main()
